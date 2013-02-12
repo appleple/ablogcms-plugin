3efbc14467e3a71a62ecd09f4b945cd72d8ae646
atsu666 <ito@appleple.com>
2013-02-12 12:00:07 +0900

    yahoo検索Apiを分離

---
+++ yahoo_search_api/js/config.js
@@ -0,0 +1,11 @@
+//--------------
+// Config.Admin
+ACMS.Config.Admin   = {
+    //--------------
+    // arg guidance
+    argGuidance :
+    {
+        'Api_Yahoo_WebSearch'   : ['bid', 'keyword', 'page'],
+        'Api_Yahoo_ImageSearch' : ['bid', 'keyword', 'page']
+    }
+}
---
+++ yahoo_search_api/php/ACMS/GET/Admin/Config/Index.php
@@ -0,0 +1,6 @@
+//--------
+// module
+$aryModule  = array(
+    'Api_Yahoo_WebSearch',
+    'Api_Yahoo_ImageSearch',
+);
